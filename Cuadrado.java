84aad476e7b4a0f7756abc914a5053bbbf48e22f
ayozeperez1407 <Ayozeperez1407rgmail.com>
2026-05-24 22:04:01 +0200

    Modificada la clase Cuadrado desde la rama develop

--- Cuadrado.java
+++ Cuadrado.java
@@ -1,6 +1,7 @@
 package figuras;
 import java.awt.Color;
 public class Cuadrado extends Rectángulo{
+// Modificado por Ayoze Perez en la rama develop
 public Cuadrado (double x, double y, Color color, double lado){
 super (x, y, color, lado, lado);
 }
